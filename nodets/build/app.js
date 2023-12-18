a59805c98406847265d4381f19facb566267ace1
mehzan07 <61779364+mehzan07@users.noreply.github.com>
2023-12-18 14:27:24 +0100

    Add files via upload
    Automation of TypeScript code app.js by Node.js

---
+++ nodets/build/app.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('Node.js TypeScript');
+console.log('Node.js Type script is automated by node.js');
+console.log('Node.js Type script automation by node.js execude the code when the code is changed');
